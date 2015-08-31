610e49a4d10c5b7ac0493327f21a10214fd4a377
Kieran O'Leary <kieranoleary@Kierans-iMac.local>
2015-08-31 12:18:33 +0100

    Subfolder creation/check if folders exist

--- ifi-ffv1.sh
+++ ifi-ffv1.sh
@@ -1,6 +1,33 @@
-#!/bin/bash 
+#!/bin/bash -x
 	
 #http://unix.stackexchange.com/questions/65510/how-do-i-append-text-to-the-beginning-and-end-of-multiple-text-files-in-bash 
+
+#http://stackoverflow.com/a/965072/2188572
+
+sourcepath="$(dirname "$1")"
+filename="$(basename "$1")"
+filenoext="${filename%.*}"
+echo $filenoext
+if [ -d "$sourcepath/$filenoext" ]; then
+	echo "It looks like these files already exist, aborting.";
+	exit 1
+else
+	mkdir "$sourcepath/$filenoext"
+	mkdir "$sourcepath/$filenoext/tmp"
+	mkdir "$sourcepath/$filenoext/provenance"
+	mkdir "$sourcepath/$filenoext/inmagic"
+	mkdir "$sourcepath/$filenoext/fixity"
+	mkdir "$sourcepath/$filenoext/video"
+fi
+provenance="$sourcepath/$filenoext/provenance"
+inmagic="$sourcepath/$filenoext/inmagic"
+fixity="$sourcepath/$filenoext/fixity"
+tmp="$sourcepath/$filenoext/tmp"
+video="$sourcepath/$filenoext/video"
+
+
+
+
 
 
 
@@ -63,15 +90,21 @@ else
 	exit 1
 fi
 
+mv "$1".framemd5 "$provenance"
+mv "$1"_output.framemd5 "$fixity"
+
+
 mediainfo -f --language=raw --output=XML "$1" > "$1_mediainfo.xml"
+mv "$1_mediainfo.xml" "$provenance"
 mediainfo -f --language=raw --output=XML "$1.mkv" > "$1.mkv_mediainfo_inmagic.xml" 
+
 mediainfo -f --language=raw --output=XML "$1.mkv" > "$1_ffv1_mediainfo.xml" 
 
 #generate qctools xml ADD AN IF STATEMENT OR A CASE SELECT- both silent and audio options enabled for now. silent ones fail if put through the audio commands
 #ffprobe -f lavfi -i "movie=$1.mkv:s=v+a[in0][in1],[in0]signalstats=stat=tout+vrep+brng,cropdetect=reset=1,split[a][b];[a]field=top[a1];[b]field=bottom[b1],[a1][b1]psnr[out0];[in1]ebur128=metadata=1[out1]" -show_frames -show_versions -of xml=x=1:q=1 -noprivate | gzip > "$1.mkv.qctools.xml.gz"
 
 ffprobe -f lavfi -i "movie=$1.mkv,signalstats=stat=tout+vrep+brng,cropdetect=reset=1,split[a][b];[a]field=top[a1];[b]field=bottom[b1],[a1][b1]psnr" -show_frames -show_versions -of xml=x=1:q=1 -noprivate | gzip > "$1.mkv.qctools.xml.gz" 
-
+mv "$1.mkv.qctools.xml.gz" "$video"
 #http://stackoverflow.com/questions/32160571/how-to-make-long-sed-script-leaner-and-more-readable-as-code/32160751?noredirect=1#comment52210665_32160751
 # remove redundant information, eg video codec showing up as PCM
 SEDSTR='s/<Codec>/<inm:Video-codec>/g'
@@ -90,7 +123,7 @@ SEDSTR="$SEDSTR;"'s/<\/DisplayAspectRatio>/<\/inm:Display-Aspect-ratio >/g'
 #this changes mmkv timecode  ; to :. Monitor this as it may mess other things up.
 SEDSTR="$SEDSTR;"'s/;/:/g'
 
-sed -i -e "$SEDSTR" "$1.mkv_mediainfo_inmagic.xml"
+sed -i.backup "$SEDSTR" "$1.mkv_mediainfo_inmagic.xml"
 
 #the first one deletes lines that do not start with in magic but doesn't work for long strings as they move to a new line. the second one deletes everything starting with inm. the ! negates the inclusion." http://stackoverflow.com/a/8068399/2188572
 sed -i '' '/^<inm/!d' "$1.mkv_mediainfo_inmagic.xml"
@@ -162,6 +195,11 @@ awk '!a[$0]++' "$1.mkv_mediainfo_inmagic.xml" > "$1.mkv_mediainfo_inmagic_final.
 
 #prints the contents of inmagic sml to terminal
 cat "$1.mkv_mediainfo_inmagic_final.xml" 
+mv "$1.mkv_mediainfo_inmagic_final.xml" "$inmagic"
+mv "$1.mkv_mediainfo_inmagic.xml" "$tmp"
+mv "$1.mkv_mediainfo_inmagic.xml.backup" "$tmp"
+mv "$1.mkv" "$video"
+mv "$1_ffv1_mediainfo.xml" "$video"
 
 echo "You should now have an xml file that can be ingested into DB/Textworks for SQL. When importing into Inmagic, DO NOT enable 'Check for matching records'"
 
